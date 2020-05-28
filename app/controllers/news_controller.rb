60639fd07500c3a12a05fbedca6a44d8f0f6efb4
Hiroharu Hara <hh.25799623@gmail.com>
2020-05-28 18:42:40 +0900

    newsコントローラにて、news_fieldファイルのインスタンス変数を用意

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,4 +1,15 @@
 class NewsController < ApplicationController
   def index
+    @current_teacher = Teacher.find(1)
+    @current_student = Student.find(1)
+    histories = LearningHistory.all
+    messages = Message.all
+    @news = []
+    histories.each do |h|
+      @news.push(h)
+    end
+    messages.each do |m|
+      @news.push(m)
+    end
   end
 end
