d927eb26185695bf6da8f7ae429bb96be3f2c185
Hiroharu Hara <hh.25799623@gmail.com>
2020-05-29 18:54:35 +0900

    teacherモデルの詳細ページを作成

---
+++ app/controllers/teachers_controller.rb
@@ -0,0 +1,11 @@
+class TeachersController < ApplicationController
+  def new
+  end
+
+  def show
+    # @current_teacher でログインしているteacherオブジェクトを取得できる。
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
   post 'sessions/create'
   delete 'sessions/destroy'
   get 'news_field', to: 'news#index'
   root 'home#top'
   get 'test', to: 'news#test'
+  get '/teachers/:id', to: "teachers#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
