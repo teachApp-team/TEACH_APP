ae112b5f3c79eda34819f2719c36de7090b82c87
Hiroharu Hara <hh.25799623@gmail.com>
2020-06-09 19:37:36 +0900

    eventのカレンダー表示を実装。

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,14 +3,15 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml { render :xml => @events }
-      format.json { render :json => @events }
-    end
+    # respond_to do |format|
+    #   format.html # index.html.erb
+    #   format.xml { render :xml => @events }
+    #   format.json { render :json => @events }
+    # end
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def new
