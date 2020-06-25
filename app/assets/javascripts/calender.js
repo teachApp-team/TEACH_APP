8ba2d4166eb147037efa02e8f1211dfcf474f1f3
Hiroharu Hara <hh.25799623@gmail.com>
2020-06-25 19:06:15 +0900

    カレンダーのイベント表示のバグを修正。

--- app/assets/javascripts/calender.js
+++ app/assets/javascripts/calender.js
@@ -199,4 +199,23 @@
 //   })
 // })
 
-
+$(".cal-stu").click(function(){
+  console.log('clickckckc')
+  console.log($(".cal-stu"))
+  console.log($(this).children('p')[0].textContent)
+  let id = $(this).children('p')[0].textContent
+  $.ajax({
+    url: "/",
+    type: "GET",
+    contentType: 'application/json',
+    dataType: "json",
+    success: function() {
+      console.log('sucess')
+      window.location.href = `/teachers/show_calender/${Number(id) + 1}`;
+    },
+    error: function() {
+      console.log('erroe')
+      window.location.href = `/teachers/show_calender/${Number(id) + 1}`;
+    }
+});
+});
