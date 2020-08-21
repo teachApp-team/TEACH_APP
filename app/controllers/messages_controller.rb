1a2e9c5507020374da14fb5d8e19f5dcd7f23c1d
Hiroharu Hara <hh.25799623@gmail.com>
2020-08-22 00:50:10 +0900

    メッセージ新規作成機能を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,6 +9,22 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
 
+  def create
+    @message = Message.new(message_params)
+    @message.teacher = @current_teacher
+    if @message.save
+      redirect_to '/news_field'
+    else
+      redirect_to '/messages/new'
+    end
+  end
+
   def edit
   end
+
+  private
+  def message_params
+    params.require(:message).permit(:comment, :student_id)
+  end
+
 end
