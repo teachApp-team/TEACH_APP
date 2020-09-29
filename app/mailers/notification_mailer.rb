163a4e9d5422f74651d20acd91a5007f07db151d
Hiroharu Hara <hh.25799623@gmail.com>
2020-09-29 19:36:28 +0900

    リプライ投稿時にメールを送信

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -22,4 +22,16 @@ class NotificationMailer < ApplicationMailer
       format.html
     end
   end
+
+  def send_reply(reply)
+    @reply = reply
+    mail(
+      subject: "#{@reply.teacher.full_name}先生から新着メッセージがあります", #メールのタイトル
+      to: @reply.student.mail,
+      # to: 'hh.25799623@gmail.com',
+      from: '"no-reply@teachapp.com" <no-reply@teachapp.com>'
+    ) do |format|
+      format.html
+    end
+  end
 end
