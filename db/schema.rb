75d086ba7b2c67c3232e555edeff559ae6c37191
Kosuke Fujita <kousuke@ezweb.ne.jp>
2020-10-04 19:57:25 +0900

    リプライに画像添付機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_122656) do
+ActiveRecord::Schema.define(version: 2020_10_04_092345) do
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "start"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 2020_09_29_122656) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "which"
+    t.string "image"
     t.index ["question_id"], name: "index_replies_on_question_id"
     t.index ["student_id"], name: "index_replies_on_student_id"
     t.index ["teacher_id"], name: "index_replies_on_teacher_id"
