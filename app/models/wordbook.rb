4552ce77a04f8b0963a85a13e992351dd7d93a98
Hiroharu Hara <hh.25799623@gmail.com>
2020-10-07 01:45:35 +0900

    単語帳とレベルの紐付けおよび表示を実装

--- app/models/wordbook.rb
+++ app/models/wordbook.rb
@@ -2,6 +2,11 @@ class Wordbook < ApplicationRecord
   has_many :words
 
   def levels
+    return [] if words.pluck(:level).blank?
     words.pluck(:level).uniq!
+  end
+
+  def leveling_words(level, limit)
+    words.where(level: level).limit(limit)
   end
 end
